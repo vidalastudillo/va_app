d4f65d26f4c3adecf4fd002245ee587531f6af43
Mauricio Vidal <13444566+maovidal@users.noreply.github.com>
2025-05-03 15:23:32 -0500

    draft: patch to rename deprecated DIAN tables

--- va_app/patches/post_install/rename_deprecated_names_dian_tables.py
+++ va_app/patches/post_install/rename_deprecated_names_dian_tables.py
@@ -14,5 +14,17 @@ def execute():
 	# 	WHERE name IN ('Salary Slip Based On Timesheet', 'Salary Slip Standard')"""
 	# )
 
-	frappe.db.rename_table("tabDIAN terceros", "tabDIAN tercero")
-	frappe.db.rename_table("tabDIAN documents", "tabDIAN document")
+	try:
+		frappe.get_doc('DocType', 'tabDIAN terceros')
+	except frappe.exceptions.DoesNotExistError:
+		print("'tabDIAN terceros' is not pressent. No need to rename it")
+	else:
+		frappe.db.rename_table("tabDIAN terceros", "tabDIAN tercero")
+
+	
+	try:
+		frappe.get_doc('DocType', 'tabDIAN documents')
+	except frappe.exceptions.DoesNotExistError:
+		print("'tabDIAN documents' is not pressent. No need to rename it")
+	else:
+		frappe.db.rename_table("tabDIAN documents", "tabDIAN document")
