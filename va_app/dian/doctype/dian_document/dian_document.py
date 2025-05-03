ff660c2c544d76b8522f5667c1a60d35804bc805
Mauricio Vidal <13444566+maovidal@users.noreply.github.com>
2025-05-03 10:47:17 -0500

    feat: DIAN module and document, tercero tables

---
+++ va_app/dian/doctype/dian_document/dian_document.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, VIDAL & ASTUDILLO Ltda and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class DIANdocument(Document):
+	pass
