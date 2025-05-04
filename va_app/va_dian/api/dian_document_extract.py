911c7f97fbce55598ee68c01664c849ba7b22244
Mauricio Vidal <13444566+maovidal@users.noreply.github.com>
2025-05-04 23:20:02 +0000

    draft: dian document extract: depuration

--- va_app/va_dian/api/dian_document_extract.py
+++ va_app/va_dian/api/dian_document_extract.py
@@ -13,24 +13,24 @@ Not functional
 
 
 import frappe
-import xml.etree.ElementTree as ET
 from frappe.utils.file_manager import get_file_path
+import xml.etree.ElementTree as ET
 from va_app.va_dian.api.dian_data_models import VA_DIAN_Document, VA_DIAN_Address, VA_DIAN_Item
 
 
 @frappe.whitelist()
-def extract_xml_info(docname) -> VA_DIAN_Document | None:
+def extract_xml_info(docname) -> dict[str, str] | None:
     """
     Provides a dictionary with information contained on the XML document
     """
 
-    # Check document exists
+    # Check requested DIAN Document exists
     doc = frappe.get_doc("DIAN document", docname)
     if not doc.xml:
         frappe.throw("No XML attachment found.")
         return None
 
-    # Get the file record
+    # Get the XML path record
     file_list = frappe.get_all("File", filters={"attached_to_doctype": "DIAN document", "attached_to_name": docname, "file_url": ("like", "%xml%")}, fields=["name", "file_name"])
     if not file_list:
         frappe.throw("Unable to locate the attached XML file.")
@@ -58,10 +58,11 @@ def extract_xml_info(docname) -> VA_DIAN_Document | None:
             return elem.text.strip() if elem is not None and elem.text else None
 
         # ######################################################################
-        # Find generic information
+        # Find generic information that should be present in any kind of
+        # electronic document
         # ######################################################################
 
-        reg_document_type = root.find('cbc:DocumentType', document_namespace)
+        document_type = get_text(root.find('cbc:DocumentType', document_namespace))
         reg_issue_date = root.find('cbc:IssueDate', document_namespace)
         reg_issue_time = root.find('cbc:IssueTime', document_namespace)
         reg_sender_party_name = root.find('cac:SenderParty/cac:PartyTaxScheme/cbc:RegistrationName', document_namespace)
@@ -90,13 +91,13 @@ def extract_xml_info(docname) -> VA_DIAN_Document | None:
 
             try:
                 # Parse the CDATA content as XML
-                invoice_root = ET.fromstring(cdata)
+                extra_document_root = ET.fromstring(cdata)
 
                 # 1. The embedded Document uses the same cbc namespace, so reuse document_namespace
-                document_uuid = get_text(invoice_root.find('.//cbc:UUID', document_namespace))
+                document_uuid = get_text(extra_document_root.find('.//cbc:UUID', document_namespace))
 
                 # 2. Sender address
-                reg_sender_address_elem = invoice_root.find('cac:AccountingSupplierParty/cac:Party/cac:PhysicalLocation/cac:Address', document_namespace)
+                reg_sender_address_elem = extra_document_root.find('cac:AccountingSupplierParty/cac:Party/cac:PhysicalLocation/cac:Address', document_namespace)
                 if reg_sender_address_elem is not None:
                     document_sender_address = VA_DIAN_Address(
                         street_name=get_text(reg_sender_address_elem.find('cac:AddressLine/cbc:Line', document_namespace)),
@@ -105,33 +106,36 @@ def extract_xml_info(docname) -> VA_DIAN_Document | None:
                         country=get_text(reg_sender_address_elem.find('cac:Country/cbc:Name', document_namespace)),
                     )
 
-                # 3. Extract item list
-                for line in invoice_root.findall('.//cac:InvoiceLine', document_namespace):
-                    line_quantity = get_text(line.find('.//cbc:InvoicedQuantity', document_namespace))
-                    line_price = get_text(line.find('.//cac:Price/cbc:PriceAmount', document_namespace))
-                    line_extension_amount = get_text(line.find('.//cbc:LineExtensionAmount', document_namespace))
-                    line_taxable_amount = get_text(line.find('.//cbc:TaxableAmount', document_namespace))
-                    line_tax_amount = get_text(line.find('.//cbc:TaxAmount', document_namespace))
-                    line_description = get_text(line.find('.//cac:Item/cbc:Description', document_namespace))
-                    if document_items is None:
-                        document_items = []
-                    document_items.append(VA_DIAN_Item(
-                        quantity=line_quantity,
-                        price=line_price,
-                        taxable_amount=line_taxable_amount,
-                        tax_amount=line_tax_amount,
-                        extension_amount=line_extension_amount,
-                        description=line_description,
-                    ))
+                # 3. Dependant on type of document
+                match document_type:
+                    case 'Contenedor de Factura Electrónica':
+                        # Extract item list
+                        for line in extra_document_root.findall('.//cac:InvoiceLine', document_namespace):
+                            line_quantity = get_text(line.find('.//cbc:InvoicedQuantity', document_namespace))
+                            line_price = get_text(line.find('.//cac:Price/cbc:PriceAmount', document_namespace))
+                            line_extension_amount = get_text(line.find('.//cbc:LineExtensionAmount', document_namespace))
+                            line_taxable_amount = get_text(line.find('.//cbc:TaxableAmount', document_namespace))
+                            line_tax_amount = get_text(line.find('.//cbc:TaxAmount', document_namespace))
+                            line_description = get_text(line.find('.//cac:Item/cbc:Description', document_namespace))
+                            if document_items is None:
+                                document_items = []
+                            document_items.append(VA_DIAN_Item(
+                                quantity=line_quantity,
+                                price=line_price,
+                                taxable_amount=line_taxable_amount,
+                                tax_amount=line_tax_amount,
+                                extension_amount=line_extension_amount,
+                                description=line_description,
+                            ))
+                    case _:
+                        frappe.throw("Unknown document type. No extra information would be produced.")
+
             except ET.ParseError:
                 pass
 
-        # match document_type:
-        #     case 'Contenedor de Factura Electrónica':
-        
         # Build object
         to_return = VA_DIAN_Document(
-            document_type = get_text(reg_document_type),
+            document_type = document_type,
             uuid = document_uuid,
             issue_date = get_text(reg_issue_date),
             issue_time = get_text(reg_issue_time),
