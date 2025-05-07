082a20295b31f384bdfea18f1e08cbfdf520f7ac
Mauricio Vidal <13444566+maovidal@users.noreply.github.com>
2025-05-07 16:43:41 +0000

    fix: tercero data on report

--- va_app/va_dian/report/gl_summary_by_account_and_party/gl_summary_by_account_and_party.py
+++ va_app/va_dian/report/gl_summary_by_account_and_party/gl_summary_by_account_and_party.py
@@ -2,7 +2,7 @@
 Copyright (c) 2024-2025, VIDAL & ASTUDILLO Ltda and contributors
 For license information, please see license.txt
 By JMVA, VIDAL & ASTUDILLO Ltda
-Version 2025-05-06
+Version 2025-05-07
 
 * PROMPT USED *
 
@@ -26,6 +26,66 @@ As I have mentioned initially, the `custom_dian_tercero` is a `custom field` of 
 
 import frappe
 from frappe import _
+from va_app.va_dian.api.dian_data_models import (
+    VA_DIAN_Tercero,
+)
+
+
+def aux_get_dian_tercero(tercero_id) -> VA_DIAN_Tercero:
+    """
+    Helper that provides a DIAN tercero object to ease result assignment.
+    """
+
+    # Retrive the record on table `DIAN tercero` for the ID
+    dian_tercero_records = frappe.db.get_all(
+        doctype='DIAN tercero',
+        fields=[
+            "razon_social",
+            "nombre_comercial",
+            "primer_apellido",
+            "segundo_apellido",
+            "primer_nombre",
+            "otros_nombres",
+            "direccion_principal",
+            "ciudad_municipio",
+            "departamento",
+            "pais",
+            "codigo_postal",
+            "correo_electronico",
+            "telefono_1",
+        ],
+        filters={"name": tercero_id},
+    )
+
+    # Check if there is a valid record returned
+    if len(dian_tercero_records)<1:
+        # No record, means an empty class is returned.
+        return VA_DIAN_Tercero(
+            nit=tercero_id,
+        )
+
+    # Select a single record from the list
+    dian_tercero_record = dian_tercero_records[0]
+
+    # Build the class content to be returned
+    to_return = VA_DIAN_Tercero(
+        nit=tercero_id,
+        razon_social=dian_tercero_record.get('razon_social'),
+        nombre_comercial=dian_tercero_record.get('nombre_comercial'),
+        primer_apellido=dian_tercero_record.get('primer_apellido'),
+        segundo_apellido=dian_tercero_record.get('segundo_apellido'),
+        primer_nombre=dian_tercero_record.get('primer_nombre'),
+        otros_nombres=dian_tercero_record.get('otros_nombres'),
+        direccion_principal=dian_tercero_record.get('direccion_principal'),
+        ciudad_municipio=dian_tercero_record.get('ciudad_municipio'),
+        departamento=dian_tercero_record.get('departamento'),
+        pais=dian_tercero_record.get('pais'),
+        codigo_postal=dian_tercero_record.get('codigo_postal'),
+        correo_electronico=dian_tercero_record.get('correo_electronico'),
+        telefono_1=dian_tercero_record.get('telefono_1'),
+    )
+    return to_return
+
 
 def execute(filters=None):
     filters = filters or {}
@@ -46,11 +106,89 @@ def execute(filters=None):
             "width": 200
         },
         {
-            "fieldname": "tercero",
+            "fieldname": "tercero_id",
             "label": _("DIAN Tercero"),
             "fieldtype": "Link",
             "options": "DIAN tercero",
-            "width": 300
+            "width": 140
+        },
+        {
+            "fieldname": "tercero_razon_social",
+            "label": _("Razón social"),
+            "fieldtype": "Text",
+            "width": 180
+        },
+        {
+            "fieldname": "tercero_nombre_comercial",
+            "label": _("Nombre comercial"),
+            "fieldtype": "Text",
+            "width": 120
+        },
+        {
+            "fieldname": "tercero_primer_apellido",
+            "label": _("Primer apellido"),
+            "fieldtype": "Text",
+            "width": 120
+        },
+        {
+            "fieldname": "tercero_segundo_apellido",
+            "label": _("Segundo apellido"),
+            "fieldtype": "Text",
+            "width": 120
+        },
+        {
+            "fieldname": "tercero_primer_nombre",
+            "label": _("Primer nombre"),
+            "fieldtype": "Text",
+            "width": 120
+        },
+        {
+            "fieldname": "tercero_otros_nombres",
+            "label": _("Otros nombres"),
+            "fieldtype": "Text",
+            "width": 120
+        },
+        {
+            "fieldname": "tercero_direccion_principal",
+            "label": _("Dirección principal"),
+            "fieldtype": "Text",
+            "width": 150
+        },
+        {
+            "fieldname": "tercero_ciudad_municipio",
+            "label": _("Ciudad/Municipio"),
+            "fieldtype": "Text",
+            "width": 120
+        },
+        {
+            "fieldname": "tercero_departamento",
+            "label": _("Departamento"),
+            "fieldtype": "Text",
+            "width": 120
+        },
+        {
+            "fieldname": "tercero_pais",
+            "label": _("País"),
+            "fieldtype": "Text",
+            "width": 120
+        },
+        {
+            "fieldname": "tercero_codigo_postal",
+            "label": _("Código postal"),
+            "fieldtype": "Text",
+            "width": 80
+        },
+        {
+            "fieldname": "tercero_correo_electronico",
+            "label": _("Correo electrónico"),
+            "fieldtype": "Text",
+            "width": 150
+        },
+        {
+            "fieldname": "tercero_telefono_1",
+            "label": _("Teléfono 1"),
+            "fieldtype": "Text",
+            "width": 120
         },
         {
             "fieldname": "total_debit",
@@ -82,29 +220,53 @@ def execute(filters=None):
         order_by="account",
 	)
 
-    # Aggregate by (account, tercero)
+    # Aggregate by (account, tercero_id)
     summary = {}
     for e in entries:
         account = e.account
-        tercero = None
+        tercero_id = None
 
-        # Resolve tercero via party document
+        # Resolve tercero_id via party document
         if e.party_type and e.party:
             # e.g. e.party_type = 'Customer', e.party = 'CUST-001'
-            tercero = frappe.get_value(e.party_type, e.party, "custom_dian_tercero")
-            # print(f"GOT value: {tercero} from identified fields")
+            tercero_id = frappe.get_value(e.party_type, e.party, "custom_dian_tercero")
+            # print(f"GOT value: {tercero_id} from identified fields")
         elif e.voucher_type == "Journal Entry" and e.voucher_no:
             # For Journal Entries, retrieve from Journal Entry record
-            tercero = frappe.get_value("Journal Entry", e.voucher_no, "custom_dian_tercero")
-            # print(f"GOT value: {tercero} from Journal")
+            tercero_id = frappe.get_value("Journal Entry", e.voucher_no, "custom_dian_tercero")
+            # print(f"GOT value: {tercero_id} from Journal")
 
         # Use None or blank if not found
-        tercero = tercero or ""
+        tercero_id = tercero_id or ""
 
-        # Key by account and tercero
-        key = (account, tercero)
+        # Key by account and tercero_id
+        key = (account, tercero_id)
+
+        # If key is not yet on the records, it is created
         if key not in summary:
-            summary[key] = {"account": account, "tercero": tercero, "total_debit": 0.0, "total_credit": 0.0, "total": 0.0}
+            # Detail about the Tercero is populated
+            tercero_detail = aux_get_dian_tercero(tercero_id)
+            summary[key] = {
+                "account": account,
+                "tercero_id": tercero_id,
+                "tercero_razon_social": tercero_detail.razon_social,
+                "tercero_nombre_comercial": tercero_detail.nombre_comercial,
+                "tercero_primer_apellido": tercero_detail.primer_apellido,
+                "tercero_segundo_apellido": tercero_detail.segundo_apellido,
+                "tercero_primer_nombre": tercero_detail.primer_nombre,
+                "tercero_otros_nombres": tercero_detail.otros_nombres,
+                "tercero_direccion_principal": tercero_detail.direccion_principal,
+                "tercero_ciudad_municipio": tercero_detail.ciudad_municipio,
+                "tercero_departamento": tercero_detail.departamento,
+                "tercero_pais": tercero_detail.pais,
+                "tercero_codigo_postal": tercero_detail.codigo_postal,
+                "tercero_correo_electronico": tercero_detail.correo_electronico,
+                "tercero_telefono_1": tercero_detail.telefono_1,
+                "total_debit": 0.0,
+                "total_credit": 0.0,
+                "total": 0.0,
+            }
+
         summary[key]["total_debit"] += e.debit
         summary[key]["total_credit"] += e.credit
         summary[key]["total"] += (e.debit - e.credit)
@@ -114,7 +276,20 @@ def execute(filters=None):
     for (acct, terc), vals in summary.items():
         data.append({
             "account": vals.get("account"),
-            "tercero": vals.get("tercero"),
+            "tercero_id": vals.get("tercero_id"),
+            "tercero_razon_social": vals.get("tercero_razon_social"),
+            "tercero_nombre_comercial": vals.get("tercero_nombre_comercial"),
+            "tercero_primer_apellido": vals.get("tercero_primer_apellido"),
+            "tercero_segundo_apellido": vals.get("tercero_segundo_apellido"),
+            "tercero_primer_nombre": vals.get("tercero_primer_nombre"),
+            "tercero_otros_nombres": vals.get("tercero_otros_nombres"),
+            "tercero_direccion_principal": vals.get("tercero_direccion_principal"),
+            "tercero_ciudad_municipio": vals.get("tercero_ciudad_municipio"),
+            "tercero_departamento": vals.get("tercero_departamento"),
+            "tercero_pais": vals.get("tercero_pais"),
+            "tercero_codigo_postal": vals.get("tercero_codigo_postal"),
+            "tercero_correo_electronico": vals.get("tercero_correo_electronico"),
+            "tercero_telefono_1": vals.get("tercero_telefono_1"),
             "total_debit": vals.get("total_debit") or 0,
             "total_credit": vals.get("total_credit") or 0,
             "total": vals.get("total") or 0
