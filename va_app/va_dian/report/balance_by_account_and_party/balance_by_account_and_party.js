c429824d393791266317a23b4070f79f0f297930
Mauricio Vidal <13444566+maovidal@users.noreply.github.com>
2025-05-07 17:03:35 +0000

    draft: balance by accoun and party report

---
+++ va_app/va_dian/report/balance_by_account_and_party/balance_by_account_and_party.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, VIDAL & ASTUDILLO Ltda and contributors
+// For license information, please see license.txt
+
+frappe.query_reports["Balance by Account and Party"] = {
+	"filters": [
+
+	]
+};
