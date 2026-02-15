8ef93d2ebadf400cada9df164d28dda256f94272
Mauricio Vidal <13444566+maovidal@users.noreply.github.com>
2026-02-15 17:29:25 +0000

    draft: testos for dian ZIP ingest

---
+++ va_app/va_dian/tests/test_dian_zip_ingest.py
@@ -0,0 +1,32 @@
+import frappe
+import os
+
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestDIANZipIngest(FrappeTestCase):
+
+    def setUp(self):
+        self.test_zip = os.path.join(
+            os.path.dirname(__file__),
+            "fixtures",
+            "sample_dian.zip",
+        )
+
+    def test_zip_ingestion_creates_document(self):
+        file_doc = frappe.get_doc({
+            "doctype": "File",
+            "file_name": "sample_dian.zip",
+            "content": open(self.test_zip, "rb").read(),
+            "is_private": 1,
+        }).insert()
+
+        from va_app.va_dian.api.dian_zip_ingest import ingest_dian_zip
+        name = ingest_dian_zip(file_doc.file_url)
+
+        doc = frappe.get_doc("DIAN document", name)
+
+        self.assertTrue(doc.xml)
+        self.assertTrue(doc.representation)
+        self.assertTrue(doc.xml_content)
+        self.assertTrue(doc.xml_dian_tercero)
